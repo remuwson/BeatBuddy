b7ad6b3f72873d5268bb0bffeff3ae3f4605d39b
niedlich3 <126873114+niedlich3@users.noreply.github.com>
2023-11-13 15:38:16 +0100

    Add files via upload

---
+++ beatBuddy_glowna.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+	<meta charset = "UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Beat Buddy</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class ="background">
+  <div class ="srodek">
+  <div class = "pole"></div>
+  <div class = "pole1"></div>
+  <div class = "pole2"></div>
+  <div class = "pole3"></div>
+  </div>
+ </div>
+
+
+</body>
+<style>
+.pole{
+	width: 100px;
+	height: 100px;
+	border: 3px solid #00FF00;
+	margin-top: 30px;
+	margin-left: 25%;
+	float: left;
+	
+}
+.pole1{
+	width: 100px;
+	height: 100px;
+	border: 3px solid #00FF00;
+	margin-top: 30px;
+	margin-left: 60%;
+	
+}
+.background{
+	width: 100%;
+	background-color: black;
+	height:750px;
+	float: left;
+	border: 1px solid black;
+}
+
+.srodek{
+	width: 75%;
+	height: 80%;
+	border: 5px solid #00FF00;
+	margin-left: 200px;
+	margin-top: 50px;
+	float: left;
+	
+}
+</style>
+</html>
