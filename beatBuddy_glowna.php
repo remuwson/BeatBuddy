0e0e0f89a63b3b0c63f11837b3b0175ce06c64c6
niedlich3 <126873114+niedlich3@users.noreply.github.com>
2023-11-13 16:12:03 +0100

    Update beatBuddy_glowna.php

--- beatBuddy_glowna.php
+++ beatBuddy_glowna.php
@@ -9,49 +9,11 @@
 <body>
 <div class ="background">
   <div class ="srodek">
-  <div class = "pole"></div>
-  <div class = "pole1"></div>
-  <div class = "pole2"></div>
-  <div class = "pole3"></div>
+  <div class= "napis"> BeatBuddy </div>
   </div>
  </div>
 
 
 </body>
-<style>
-.pole{
-	width: 100px;
-	height: 100px;
-	border: 3px solid #00FF00;
-	margin-top: 30px;
-	margin-left: 25%;
-	float: left;
-	
-}
-.pole1{
-	width: 100px;
-	height: 100px;
-	border: 3px solid #00FF00;
-	margin-top: 30px;
-	margin-left: 60%;
-	
-}
-.background{
-	width: 100%;
-	background-color: black;
-	height:750px;
-	float: left;
-	border: 1px solid black;
-}
 
-.srodek{
-	width: 75%;
-	height: 80%;
-	border: 5px solid #00FF00;
-	margin-left: 200px;
-	margin-top: 50px;
-	float: left;
-	
-}
-</style>
 </html>
