f7197c8d8ab253661dcb1a58c811223d93d4d9b1
niedlich3 <126873114+niedlich3@users.noreply.github.com>
2023-11-14 14:02:43 +0100

    Update beatBuddy_glowna.php

--- beatBuddy_glowna.php
+++ beatBuddy_glowna.php
@@ -10,12 +10,19 @@
 <div class ="background">
   <div class ="srodek">
   <div class= "logo"> BeatBuddy </div>
- 
-   <p class="suwak1">20<input type="range" id ="suwak" name="slider" min="20" max="50" step="1" value="50">50</div></p>
+  <div class ="opis1"> Wybierz ilość piosenek, którą chcesz żeby była na twojej playliście:</div>
+   <p class="suwak1">20<input type="range" id ="suwak" name="slider" min="20" max="50" step="1" value="35">50</div></p>
+	
   </div>
  </div>
 
 
 </body>
-
+<script>
+const text_2 = document.getElementById('suwak');
+        const output_2 = document.getElementById('suwak');
+        text_2.addEventListener('input', function () {
+            output_2.textContent = `Grubość Bordera : ${text_2.value}px`;
+        });
+		</script>
 </html>
