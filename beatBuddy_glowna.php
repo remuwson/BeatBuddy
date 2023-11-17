7bce74dbde9c5d48d8f3e022752dcb6008be620a
niedlich3 <126873114+niedlich3@users.noreply.github.com>
2023-11-17 11:07:32 +0100

    Update beatBuddy_glowna.php

--- beatBuddy_glowna.php
+++ beatBuddy_glowna.php
@@ -11,10 +11,11 @@
   <div class ="srodek">
   <div class= "logo"> BeatBuddy </div>
   <div class ="opis1"> Wybierz ilość piosenek, którą chcesz żeby była na twojej playliście:</div>
-   <p class="suwak1">20<input type="range" id ="suwak" name="slider" min="20" max="50" step="1" value="35">50</div></p>
-	
+   <p class="suwak1">20<input type="range" id ="suwak" name="slider" min="20" max="50" step="1" value="35">50<p>Wartość suwaka: <span id="sliderValue">50</span></p></p>
+	<p class ="napis_do_suwaka">Ilość piosenek: <span id="sliderValue"></span></p>
+</div>
   </div>
- </div>
+ 
 
 
 </body>
@@ -24,5 +25,13 @@ const text_2 = document.getElementById('suwak');
         text_2.addEventListener('input', function () {
             output_2.textContent = `Grubość Bordera : ${text_2.value}px`;
         });
+		var slider = document.getElementById("suwak");
+    var output = document.getElementById("sliderValue");
+
+    // Zaktualizuj wartość przy każdej zmianie suwaka
+    slider.addEventListener("input", function() {
+      output.innerHTML = this.value;
+    });
+		
 		</script>
 </html>
