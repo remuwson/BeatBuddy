d373a2e297ec032b0dde28deb376780b0ec2ff26
remuwson <126872831+remuwson@users.noreply.github.com>
2023-11-19 15:01:36 +0100

    1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
  const text_2 = document.getElementById('suwak');
-        const output_2 = document.getElementById('suwak');
+        const output_2 = document.getElementById('sliderValue');
         text_2.addEventListener('input', function () {
             output_2.textContent = `Grubość Bordera : ${text_2.value}px`;
         });
