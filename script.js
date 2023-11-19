7f4d261808d3c853ee811574362b61c5ba162de5
remuwson <126872831+remuwson@users.noreply.github.com>
2023-11-19 15:02:37 +0100

    1

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
- const text_2 = document.getElementById('suwak');
-        const output_2 = document.getElementById('sliderValue');
-        text_2.addEventListener('input', function () {
-            output_2.textContent = `Grubość Bordera : ${text_2.value}px`;
-        });
-		let slider = document.getElementById("suwak");
-    let output = document.getElementById("sliderValue");
-
-    // Zaktualizuj wartość przy każdej zmianie suwaka
-    slider.addEventListener("input", function() {
-      output.innerHTML = this.value;
-       });
+document.addEventListener('DOMContentLoaded', function() {
+    var slider = document.getElementById('mySlider');
+    var sliderValue = document.getElementById('slider-value');
+  
+    // Funkcja aktualizująca wartość nad suwakiem
+    function updateSliderValue() {
+      var value = slider.value;
+      sliderValue.textContent = value;
+    }
+  
+    // Dodanie obsługi zdarzenia dla przesuwania suwaka
+    slider.addEventListener('input', function() {
+      updateSliderValue();
+    });
+  
+    // Inicjalne ustawienie wartości
+    updateSliderValue();
+  });
